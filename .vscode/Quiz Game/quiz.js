512ea7466ca6f6160003d291ec0dc674780bc726
AnushreeDate <anushreedate0@gmail.com>
2024-04-16 18:39:03 +0530

    16 april

---
+++ .vscode/Quiz Game/quiz.js
@@ -0,0 +1,33 @@
+/*let data =[
+    {
+       Qu. js is used for?
+       A:  "Animation",
+       B:  "Frontend"
+       C:  "Backend"
+       D:  "None of these"
+    },
+
+    {
+        Qu. js is used for?
+        A:  "Animation",
+        B:  "Frontend"
+        C:  "Backend"
+        D:  "None of these"
+     },
+
+     {
+        Qu. js is used for?
+        A:  "Animation",
+        B:  "Frontend"
+        C:  "Backend"
+        D:  "None of these"
+     },
+
+     {
+        Qu. js is used for?
+        A:  "Animation",
+        B:  "Frontend"
+        C:  "Backend"
+        D:  "None of these"
+     },
+]*/
